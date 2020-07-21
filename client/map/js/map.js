07e0a22ccfab9b57d12b2c4990ab41b3aeb7ba2f
fullonic <somnium@riseup.net>
2020-07-21 17:25:02 +0200

    Add contro layer to map

--- client/map/js/map.js
+++ client/map/js/map.js
@@ -1,14 +1,219 @@
+var geojson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "El Cogull\u00f3 de Cabra",
+        alt: 881,
+        essential: true,
+      },
+      geometry: { type: "Point", coordinates: [1.30575794888, 41.4290355033] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Puig de Tretzevents", alt: 2731, essential: true },
+      geometry: { type: "Point", coordinates: [2.4683643295, 42.492101894] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Torre de Madeloc", alt: 656, essential: true },
+      geometry: { type: "Point", coordinates: [3.07511682009, 42.4904284262] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Cambra dAse", alt: 2750, essential: true },
+      geometry: { type: "Point", coordinates: [2.12512019092, 42.4526124454] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Canig\u00f3", alt: 2784, essential: true },
+      geometry: { type: "Point", coordinates: [2.45655964191, 42.518901928] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Carlit", alt: 2921, essential: true },
+      geometry: { type: "Point", coordinates: [1.93149892949, 42.5707470717] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Puig Peric", alt: 2810, essential: true },
+      geometry: { type: "Point", coordinates: [1.98444924692, 42.6136270709] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Roc de Madres", alt: 2469, essential: true },
+      geometry: { type: "Point", coordinates: [2.19240962976, 42.6518175066] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Torre dEina", alt: 2830, essential: true },
+      geometry: { type: "Point", coordinates: [2.14714732054, 42.4344424965] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Casamanya Nord", alt: 2750, essential: true },
+      geometry: { type: "Point", coordinates: [1.5703040881, 42.5873416545] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Comapedrosa", alt: 2942, essential: true },
+      geometry: { type: "Point", coordinates: [1.44365450486, 42.5918057015] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Pic de la Serrera", alt: 2912, essential: true },
+      geometry: { type: "Point", coordinates: [1.602514145, 42.625535127] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Pic del Vent", alt: 816, essential: true },
+      geometry: { type: "Point", coordinates: [2.12829694974, 41.6553025564] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Pic Negre dEnvalira", alt: 2822, essential: true },
+      geometry: { type: "Point", coordinates: [1.72144258547, 42.5172544705] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Sant Sadurn\u00ed de Gallifa",
+        alt: 942,
+        essential: true,
+      },
+      geometry: { type: "Point", coordinates: [2.12295683181, 41.702765106] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Torres de Cabren\u00e7",
+        alt: 1344,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [2.5421728176, 42.3645718057] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Puig de Sant Cristau", alt: 1015, essential: false },
+      geometry: { type: "Point", coordinates: [2.89069878728, 42.4920219845] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Torre del Far", alt: 498, essential: false },
+      geometry: { type: "Point", coordinates: [2.76180548558, 42.8054119343] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Roc de Mallorca", alt: 443, essential: false },
+      geometry: { type: "Point", coordinates: [2.7114749872, 42.6136853093] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Montolier de Perell\u00f3s",
+        alt: 707,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [2.86518569768, 42.9185248623] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "For\u00e7a Real", alt: 507, essential: false },
+      geometry: { type: "Point", coordinates: [2.70022626173, 42.7269198717] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Tuc Dormidor", alt: 1843, essential: false },
+      geometry: { type: "Point", coordinates: [2.26602, 42.71035] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Roca Gelera", alt: 1110, essential: false },
+      geometry: { type: "Point", coordinates: [2.43770677452, 42.6851098962] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Puig de lEstella", alt: 1778, essential: false },
+      geometry: { type: "Point", coordinates: [2.55355631584, 42.5146828414] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Puig de les Tres Esteles",
+        alt: 2099,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [2.32321679045, 42.508771955] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Puig de Gallin\u00e0s",
+        alt: 2624,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [2.2153093651, 42.4777793254] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Pic de Rac\u00f3 Gros",
+        alt: 2782,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [2.1871139362, 42.4322202523] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Pic de lInfern", alt: 2870, essential: false },
+      geometry: { type: "Point", coordinates: [2.21373246579, 42.4239313658] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Tur\u00f3 de Bell-lloc",
+        alt: 1702,
+        essential: false,
+      },
+      geometry: { type: "Point", coordinates: [1.92218442466, 42.4832775498] },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Roc del Boc", alt: 2774, essential: false },
+      geometry: { type: "Point", coordinates: [2.15515749301, 42.4463355484] },
+    },
+  ],
+};
+
+// Map configuration setup
 const terrain_tails =
   "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png";
 const attribution =
   "Map tiles by <a href='http://stamen.com'>Stamen Design</a>, under <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a>. Data by <a href='http://openstreetmap.org'>OpenStreetMap</a>, under <a href='http://www.openstreetmap.org/copyright'>ODbL</a>";
 
 const osm_tiles = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}";
-var mymap = L.map("mapid").setView([41.0, 1.3], 8);
+
+// Layers
+var essential = L.layerGroup([]);
+var repte = L.layerGroup([]);
+
+var overlayMaps = {
+  Essential: essential,
+  Repte: repte,
+};
+
+// Base map
+var map = L.map("mapid", {
+  center: [41.0, 1.3],
+  zoom: 8,
+  layers: [essential, repte], //default selected layers
+});
+
 L.tileLayer(terrain_tails, {
-  foo: "bar", attribution: attribution,
+  foo: "bar",
+  attribution: attribution,
   maxZoom: 18,
-  // tileSize: 5,
-}).addTo(
-  mymap
-);
+}).addTo(map);
+
+L.control.layers({}, overlayMaps).addTo(map);
